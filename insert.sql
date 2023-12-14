e87f8dedbf92a06cf47585014d5dc3055d6eaceb
NatalijaGlavas <149605415+NatalijaGlavas@users.noreply.github.com>
2023-12-14 20:58:24 +0100

    Unos podataka u tablicu smjer

---
+++ insert.sql
@@ -0,0 +1,32 @@
+use edunovawp3;
+
+select * from smjerovi;
+
+insert into smjerovi (naziv, trajanje, cijena, vaucer)
+values ('Web programiranje', 225, 1859.45, 1);
+
+
+-- losa sintaksa
+insert into smjerovi values
+('Web dizajn', null, null, null)
+
+insert into smjerovi (naziv) values ('čžćđ ĆŽĆĐŽ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
